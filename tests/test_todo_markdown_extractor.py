040aef31ce9f01297df16d44935fcfb762027ae5
Krystian Safjan <ksafjan@gmail.com>
2022-06-22 09:02:28 +0200

    Added tests

---
+++ tests/test_todo_markdown_extractor.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from todo_markdown_extractor import markdown_extractor
+
+
+class Test(TestCase):
+    def test_markdown_extractor__bare(self):
+        markdown_extractor(filename="text_with_todos.md", stats=False, sections=False)
+
+    def test_markdown_extractor__with_stats(self):
+        markdown_extractor(filename="text_with_todos.md", stats=True, sections=False)
+
+    def test_markdown_extractor__with_sections(self):
+        markdown_extractor(filename="text_with_todos.md", stats=False, sections=True)
+
+    def test_markdown_extractor__with_stats_and_sections(self):
+        markdown_extractor(filename="text_with_todos.md", stats=True, sections=True)
