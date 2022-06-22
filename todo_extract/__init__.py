7d628845f7b7ea50eee1782ddfd12a8ea74d2121
Krystian Safjan <ksafjan@gmail.com>
2022-06-22 10:08:53 +0200

    Added package files

---
+++ todo_extract/__init__.py
@@ -0,0 +1,4 @@
+from .todo_extract import markdown_extractor
+from .version import __version__
+
+__all__ = ["markdown_extractor"]
---
+++ todo_extract/main.py
@@ -0,0 +1,34 @@
+import argparse
+
+from . import version
+from .todo_extract import markdown_extractor
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Extract todo items from markdown file"
+    )
+
+    parser.add_argument("file_name", help="markdown file path")
+
+    parser.add_argument(
+        "-s",
+        "--stats",
+        action="store_true",
+        help="display stats",
+        default=False,
+    )
+
+    parser.add_argument(
+        "-c",
+        "--chapters",
+        action="store_true",
+        help="display items grouped by important/not important and done/not done",
+        default=False,
+    )
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {version.__version__}"
+    )
+
+    args = parser.parse_args()
+    markdown_extractor(args.file_name, args.stats, args.chapters)
